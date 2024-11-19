27f14c5ce94011182b9eb738b1b060fbf88b9091
Kesari-PrasannaRao <kesariprasannarao1717@gmail.com>
2024-11-19 17:55:33 +0530

    Create Diamond_Pattern.py

---
+++ visaa_prep/Diamond_Pattern.py
@@ -0,0 +1,16 @@
+n=int(input())
+k=0
+for i in range(n):
+    for j in range(n):
+        if j<=i:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+for i in range(n-1):
+    for j in range(n):
+        if j<n-i-1:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
